3747ccd9d8cf36f1c09787c0aeaf80bb0286279b
Colton Freitas <freitacr@plu.edu>
2019-05-16 15:33:24 -0700

    Add project files.

---
+++ Mechanic's Assistant Client/src/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Mechanics_Assistant_Client.Forms;
+
+namespace Mechanics_Assistant_Client
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MechanicsAssistant());
+            Application.Run(new MechanicsAssistantOutputForm());
+        }
+    }
+}
---
+++ Mechanic's Assistant Client/src/forms/MechanicsAssistantMainform.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mechanics_Assistant_Client.Forms
+{
+    public partial class MechanicsAssistant : Form
+    {
+
+        private bool MakeBoxHasShadowText = true;
+        private bool ModelBoxHasShadowText = true;
+        private bool ComplaintBoxHasShadowText = true;
+
+        private string MakeBoxShadowText = "Please Enter Vehicle's Make...";
+        private string ModelBoxShadowText = "Please Enter Vehicle's Model...";
+        private string ComplaintBoxShadowText = "Please Enter Customer's Complaint...";
+
+
+        public MechanicsAssistant()
+        {
+            InitializeComponent();
+        }
+
+
+        public void MakeTextBoxClick(object sender, MouseEventArgs eventArgs)
+        {
+            if (MakeBoxHasShadowText)
+            {
+                MakeEntry.ResetText();
+                MakeBoxHasShadowText = false;
+            }
+        }
+
+        public void ModelTextBoxClick(object sender, MouseEventArgs eventArgs)
+        {
+            if (ModelBoxHasShadowText)
+            {
+                ModelTextBox.ResetText();
+                ModelBoxHasShadowText = false;
+            }
+        }
+
+        public void ComplaintTextBoxClick(object sender, MouseEventArgs eventArgs)
+        {
+            if (ComplaintBoxHasShadowText)
+            {
+                ComplaintTextBox.ResetText();
+                ComplaintBoxHasShadowText = false;
+            }
+        }
+
+        public void QueryButtonClick(object sender, EventArgs eventArgs)
+        {
+            this.Close();
+        }
+
+        private void MakeTextBoxLeaveFocus(object sender, EventArgs e)
+        {
+            if (MakeEntry.Text == "")
+            {
+                MakeEntry.Text = MakeBoxShadowText;
+                MakeBoxHasShadowText = true;
+            }
+        }
+
+        private void ModelTextBoxLeaveFocus(object sender, EventArgs e)
+        {
+            if (ModelTextBox.Text == "")
+            {
+                ModelTextBox.Text = ModelBoxShadowText;
+                ModelBoxHasShadowText = true;
+            }
+        }
+
+        private void ComplaintTextBoxLeaveFocus(object sender, EventArgs e)
+        {
+            if (ComplaintTextBox.Text == "")
+            {
+                ComplaintTextBox.Text = ComplaintBoxShadowText;
+                ComplaintBoxHasShadowText = true;
+            }
+        }
+    }
+}
