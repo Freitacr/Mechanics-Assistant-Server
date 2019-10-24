7d490be8b655922b5d8a444c7bbd67c0d8912b85
careyjw <careyjw@plu.edu>
2019-10-24 13:59:47 -0700

    Implement CompanyRequests POST

--- Mechanics Assistant Server/Net/Api/CompanyRequestsApi.cs
+++ Mechanics Assistant Server/Net/Api/CompanyRequestsApi.cs
@@ -37,9 +37,72 @@ namespace OldManInTheShopServer.Net.Api
         }
         private void HandlePostRequest(HttpListenerContext ctx)
         {
+            try
+            {
+                if (!ctx.Request.HasEntityBody)
+                {
+                    WriteBodyResponse(ctx, 400, "Bad Request", "No Body");
+                    return;
+                }
+                CompanyRequestsApiFullPostRequest entry = JsonDataObjectUtil<CompanyRequestsApiFullPostRequest>.ParseObject(ctx);
+                if (ValidateFullPostRequest(entry))
+                {
+                    WriteBodyResponse(ctx, 400, "Bad Request", "Incorrect Format");
+                    return;
+                }
+                MySqlDataManipulator connection = new MySqlDataManipulator();
+                using (connection)
+                {
+                    bool res = connection.Connect(MySqlDataManipulator.GlobalConfiguration.GetConnectionString());
+                    if (!res)
+                    {
+                        WriteBodyResponse(ctx, 500, "Unexpected Server Error", "Connection to database failed");
+                        return;
+                    }
+                    OverallUser mappedUser = connection.GetUserById(entry.UserId);
+                    if (mappedUser == null)
+                    {
+                        WriteBodyResponse(ctx, 404, "Not Found", "User was not found on on the server");
+                        return;
+                    }
+                    if (!UserVerificationUtil.LoginTokenValid(mappedUser, entry.LoginToken))
+                    {
+                        WriteBodyResponse(ctx, 401, "Not Authorized", "Login token was incorrect.");
+                        return;
+                    }
+                    if (!UserVerificationUtil.AuthTokenValid(mappedUser, entry.AuthToken))
+                    {
+                        WriteBodyResponse(ctx, 401, "Not Authorized", "Auth token was ezpired or incorrect");
+                        return;
+                    }
 
+                    res = connection.AddJoinRequest(entry.CompanyId,entry.UserId);
+                    if (!res)
+                    {
+                        WriteBodyResponse(ctx,500,"Unexpected Server Error", connection.LastException.Message);
+                        return;
+                    }
+                    WriteBodylessResponse(ctx,200,"OK");
+                }
+            }
+            catch (Exception e)
+            {
+                WriteBodyResponse(ctx, 500, "Internal Server Error", e.Message);
+            }
         }
 
+        private bool ValidateFullPostRequest(CompanyRequestsApiFullPostRequest req)
+        {
+            if (req.UserId == -1)
+                return false;
+            if (req.CompanyId == -1)
+                return false;
+            if (req.LoginToken == "")
+                return false;
+            if (req.AuthToken == "")
+                return false;
+            return true;
+        }
         private void HandleGetRequest(HttpListenerContext ctx)
         {
 
