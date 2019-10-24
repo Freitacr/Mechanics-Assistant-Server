ac92814690c6ade71737b197939d4c647d7e6661
careyjw <careyjw@plu.edu>
2019-10-24 14:28:05 -0700

    Implement CompanyPartsList POST

--- Mechanics Assistant Server/Net/Api/CompanyPartslistsRequestApi.cs
+++ Mechanics Assistant Server/Net/Api/CompanyPartslistsRequestApi.cs
@@ -11,7 +11,7 @@ using OldManInTheShopServer.Util;
 namespace OldManInTheShopServer.Net.Api
 {
     [DataContract]
-    class CompanyPartslistsApiFullPostRequest
+    class CompanyPartslistsRequestApiFullPostRequest
     {
         [DataMember]
         public int UserId;
@@ -23,8 +23,6 @@ namespace OldManInTheShopServer.Net.Api
         public int RepairJobId;
         [DataMember]
         public string RequiredPartsList;
-        [DataMember]
-        public int Year;
         [DataMember]
         public int CompanyId;
     }
@@ -44,9 +42,77 @@ namespace OldManInTheShopServer.Net.Api
         }
         private void HandlePostRequest(HttpListenerContext ctx)
         {
+            try
+            {
+                if (!ctx.Request.HasEntityBody)
+                {
+                    WriteBodyResponse(ctx, 400, "Bad Request", "No Body");
+                    return;
+                }
+                CompanyPartslistsRequestApiFullPostRequest entry = JsonDataObjectUtil<CompanyPartslistsRequestApiFullPostRequest>.ParseObject(ctx);
+                if (ValidateFullPostRequest(entry))
+                {
+                    WriteBodyResponse(ctx, 400, "Bad Request", "Incorrect Format");
+                    return;
+                }
+                MySqlDataManipulator connection = new MySqlDataManipulator();
+                using (connection)
+                {
+                    bool res = connection.Connect(MySqlDataManipulator.GlobalConfiguration.GetConnectionString());
+                    if (!res)
+                    {
+                        WriteBodyResponse(ctx, 500, "Unexpected Server Error", "Connection to database failed");
+                        return;
+                    }
+                    OverallUser mappedUser = connection.GetUserById(entry.UserId);
+                    if (mappedUser == null)
+                    {
+                        WriteBodyResponse(ctx, 404, "Not Found", "User was not found on on the server");
+                        return;
+                    }
+                    if (!UserVerificationUtil.LoginTokenValid(mappedUser, entry.LoginToken))
+                    {
+                        WriteBodyResponse(ctx, 401, "Not Authorized", "Login token was incorrect.");
+                        return;
+                    }
+                    if (!UserVerificationUtil.AuthTokenValid(mappedUser, entry.AuthToken))
+                    {
+                        WriteBodyResponse(ctx, 401, "Not Authorized", "Auth token was ezpired or incorrect");
+                        return;
+                    }
 
+                    RequirementAdditionRequest request = new RequirementAdditionRequest(entry.UserId,entry.RepairJobId,entry.RequiredPartsList);
+                    res = connection.AddPartsListAdditionRequest(entry.CompanyId,request);
+                    if (!res)
+                    {
+                        WriteBodyResponse(ctx,500,"Unexpected Server Error",connection.LastException.Message);
+                        return;
+                    }
+                    WriteBodylessResponse(ctx, 200, "OK");
+                }
+            }
+            catch (Exception e)
+            {
+                WriteBodyResponse(ctx, 500, "Internal Server Error", e.Message);
+            }
         }
 
+        private bool ValidateFullPostRequest(CompanyPartslistsRequestApiFullPostRequest req)
+        {
+            if (req.UserId == -1)
+                return false;
+            if (req.CompanyId == -1)
+                return false;
+            if (req.RepairJobId == -1)
+                return false;
+            if (req.LoginToken == "")
+                return false;
+            if (req.AuthToken == "")
+                return false;
+            if (req.RequiredPartsList == "")
+                return false;
+            return true;
+        }
         private void HandleGetRequest(HttpListenerContext ctx)
         {
 
