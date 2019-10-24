64c906a7ef914184317732902a7c3ae350558a37
careyjw <careyjw@plu.edu>
2019-10-24 14:16:41 -0700

    Implement SafetyRequest POST

--- Mechanics Assistant Server/Net/Api/CompanySafetyRequestApi.cs
+++ Mechanics Assistant Server/Net/Api/CompanySafetyRequestApi.cs
@@ -42,9 +42,79 @@ namespace OldManInTheShopServer.Net.Api
         }
         private void HandlePostRequest(HttpListenerContext ctx)
         {
+            try
+            {
+                if (!ctx.Request.HasEntityBody)
+                {
+                    WriteBodyResponse(ctx, 400, "Bad Request", "No Body");
+                    return;
+                }
+                CompanySafetyRequestApiFullPostRequest entry = JsonDataObjectUtil<CompanySafetyRequestApiFullPostRequest>.ParseObject(ctx);
+                if (ValidateFullPostRequest(entry))
+                {
+                    WriteBodyResponse(ctx, 400, "Bad Request", "Incorrect Format");
+                    return;
+                }
+                MySqlDataManipulator connection = new MySqlDataManipulator();
+                using (connection)
+                {
+                    bool res = connection.Connect(MySqlDataManipulator.GlobalConfiguration.GetConnectionString());
+                    if (!res)
+                    {
+                        WriteBodyResponse(ctx, 500, "Unexpected Server Error", "Connection to database failed");
+                        return;
+                    }
+                    OverallUser mappedUser = connection.GetUserById(entry.UserId);
+                    if (mappedUser == null)
+                    {
+                        WriteBodyResponse(ctx, 404, "Not Found", "User was not found on on the server");
+                        return;
+                    }
+                    if (!UserVerificationUtil.LoginTokenValid(mappedUser, entry.LoginToken))
+                    {
+                        WriteBodyResponse(ctx, 401, "Not Authorized", "Login token was incorrect.");
+                        return;
+                    }
+                    if (!UserVerificationUtil.AuthTokenValid(mappedUser, entry.AuthToken))
+                    {
+                        WriteBodyResponse(ctx, 401, "Not Authorized", "Auth token was ezpired or incorrect");
+                        return;
+                    }
 
+                    //build safety request
+                    RequirementAdditionRequest request = new RequirementAdditionRequest(entry.UserId, entry.RepairJobId, entry.SafetyRquirements);
+                    //send request
+                    res = connection.AddSafetyAdditionRequest(entry.CompanyId,request);
+                    if (!res)
+                    {
+                        WriteBodyResponse(ctx, 500, "Unexpected Serer Error", connection.LastException.Message);
+                        return;
+                    }
+                    WriteBodylessResponse(ctx,200,"OK");
+                }
+            }
+            catch (Exception e)
+            {
+                WriteBodyResponse(ctx, 500, "Internal Server Error", e.Message);
+            }
         }
 
+        private bool ValidateFullPostRequest(CompanySafetyRequestApiFullPostRequest req)
+        {
+            if (req.UserId == -1)
+                return false;
+            if (req.CompanyId == -1)
+                return false;
+            if (req.AuthToken == "")
+                return false;
+            if (req.LoginToken == "")
+                return false;
+            if (req.RepairJobId == -1)
+                return false;
+            if (req.SafetyRquirements == "")
+                return false;
+            return true;
+        }
         private void HandleGetRequest(HttpListenerContext ctx)
         {
 
