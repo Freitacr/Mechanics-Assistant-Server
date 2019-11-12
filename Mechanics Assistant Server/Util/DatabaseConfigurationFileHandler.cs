4ea4325af02243c83dcb4f946ab399e697a39035
Freitacr <freitacr@plu.edu>
2019-11-12 13:04:11 -0800

    Revert "Revert "Update Passwords to be inside SecureStrings""
    This reverts commit e1823c6d66c6db250e8538443baaae6e38d4bc50.

--- Mechanics Assistant Server/Util/DatabaseConfigurationFileHandler.cs
+++ Mechanics Assistant Server/Util/DatabaseConfigurationFileHandler.cs
@@ -6,6 +6,7 @@ using EncodingUtilities;
 using System.Runtime.Serialization.Json;
 using System.Runtime.Serialization;
 using System.Text;
+using System.Security;
 
 namespace OldManInTheShopServer.Util
 {
@@ -22,7 +23,49 @@ namespace OldManInTheShopServer.Util
         public string Host;
 
         [DataMember(IsRequired = false, EmitDefaultValue = false)]
-        public string Pass;
+        public SecureString Pass;
+
+        public string Serialize()
+        {
+            StringBuilder retBuilder = new StringBuilder();
+            if(Pass != null)
+            {
+                retBuilder.Append("Pass:" + Pass.ConvertToString() + ';');
+            }
+            retBuilder.Append("Database:" + Database + ';');
+            retBuilder.Append("User:" + User + ';');
+            retBuilder.Append("Host:" + Host + ";");
+            return retBuilder.ToString();
+        }
+
+        public static DatabaseConfigurationFileContents Deserialize(string contentsIn)
+        {
+            DatabaseConfigurationFileContents ret = new DatabaseConfigurationFileContents();
+            var contentSplit = contentsIn.Split(';');
+            foreach(string field in contentSplit)
+            {
+                var fieldSplit = field.Split(':');
+                
+                if(fieldSplit[0].StartsWith("P"))
+                {
+                    ret.Pass = new SecureString();
+                    foreach(char c in fieldSplit[1])
+                    {
+                        ret.Pass.AppendChar(c);
+                    }
+                } else if (fieldSplit[0].StartsWith("D"))
+                {
+                    ret.Database = fieldSplit[1];
+                } else if (fieldSplit[0].StartsWith("U"))
+                {
+                    ret.User = fieldSplit[1];
+                } else if (fieldSplit[0].StartsWith("H"))
+                {
+                    ret.Host = fieldSplit[1];
+                }
+            }
+            return ret;
+        }
     }
 
     static class DatabaseConfigurationFileHandler
@@ -31,8 +74,8 @@ namespace OldManInTheShopServer.Util
 
         public static DatabaseConfigurationFileContents LoadConfigurationFile()
         {
-            ANSEncodingLib.AnsBlockDecoder fileDecoder;
-            fileDecoder = new ANSEncodingLib.AnsBlockDecoder(new MemoryStream());
+            AnsBlockDecoder fileDecoder;
+            fileDecoder = new AnsBlockDecoder(new MemoryStream());
             FileStream fileReader;
             try
             {
@@ -48,7 +91,7 @@ namespace OldManInTheShopServer.Util
                 fileDecoder.Output.Close();
                 byte[] fileBytes = ((MemoryStream)fileDecoder.Output).ToArray();
                 fileContents = Encoding.UTF8.GetString(fileBytes);
-                return JsonDataObjectUtil<DatabaseConfigurationFileContents>.ParseObject(fileContents);
+                return DatabaseConfigurationFileContents.Deserialize(fileContents);
             }
         }
 
@@ -64,7 +107,7 @@ namespace OldManInTheShopServer.Util
 
         public static bool WriteConfigurationFile(DatabaseConfigurationFileContents fileContents)
         {
-            string contents = JsonDataObjectUtil<DatabaseConfigurationFileContents>.ConvertObject(fileContents);
+            string contents = fileContents.Serialize();
             if (contents == null)
                 return false;
             AnsBlockEncoder fileWriter;
