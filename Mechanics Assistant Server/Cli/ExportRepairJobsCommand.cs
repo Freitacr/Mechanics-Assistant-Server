93f5296a1724cfbc32a6cd9173303d31aac04e9a
Freitacr <freitacr@plu.edu>
2019-11-14 12:11:45 -0800

    Implement Import and Export of RepairJobs

---
+++ Mechanics Assistant Server/Cli/ExportRepairJobsCommand.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OldManInTheShopServer.Attribute;
+using OldManInTheShopServer.Data.MySql;
+using OldManInTheShopServer.Data;
+using OldManInTheShopServer.Data.MySql.TableDataTypes;
+using System.IO;
+using OldManInTheShopServer.Util;
+
+namespace OldManInTheShopServer.Cli
+{
+    class ExportRepairJobsCommand : CommandLineCommand
+    {
+        [KeyedArgument("-e", true, "repairjob")]
+        public string Flag;
+
+        [PositionalArgument(0)]
+        public string FilePath;
+
+        public override void PerformFunction(MySqlDataManipulator manipulator)
+        {
+            FileSystemDataSource dataSource = new FileSystemDataSource();
+            List<MechanicQuery> queries = dataSource.LoadMechanicQueries();
+            List<JobDataEntry> toWrite = new List<JobDataEntry>();
+            foreach(MechanicQuery query in queries)
+            {
+                JobDataEntry toAdd = new JobDataEntry()
+                {
+                    Make = query.Make,
+                    Model = query.Model,
+                    Complaint = query.Complaint,
+                    Problem = query.Problem,
+                    JobId = "Unknown",
+                    Year = -1
+                };
+                toWrite.Add(toAdd);
+            }
+
+            StreamWriter fileWriter = new StreamWriter(FilePath); //This is a CLI for devs, so no worries if this goes wonky
+            fileWriter.WriteLine('[');
+            
+
+            foreach(JobDataEntry entry in toWrite)
+            {
+                string entryJson = JsonDataObjectUtil<JobDataEntry>.ConvertObject(entry);
+                fileWriter.WriteLine(entryJson);
+            }
+            fileWriter.WriteLine(']');
+            fileWriter.Close();
+        }
+    }
+}
---
+++ Mechanics Assistant Server/Cli/ImportRepairJobsCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using OldManInTheShopServer.Attribute;
+using OldManInTheShopServer.Data.MySql;
+using OldManInTheShopServer.Data.MySql.TableDataTypes;
+using OldManInTheShopServer.Util;
+
+namespace OldManInTheShopServer.Cli
+{
+    class ImportRepairJobsCommand : CommandLineCommand
+    {
+        [KeyedArgument("-i", true, "repairjob")]
+        public string Flag;
+
+        [PositionalArgument(0)]
+        public string FilePath;
+
+        [PositionalArgument(1)]
+        public int CompanyId;
+
+        public override void PerformFunction(MySqlDataManipulator manipulator)
+        {
+            StreamReader fileReader = new StreamReader(FilePath);
+            string jsonString = fileReader.ReadToEnd();
+            fileReader.Close();
+            List<JobDataEntry> loadedData = JsonDataObjectUtil<List<JobDataEntry>>.ParseObject(jsonString);
+            foreach (JobDataEntry entry in loadedData)
+            {
+                entry.Make = entry.Make.ToLower();
+                entry.Model = entry.Model.ToLower();
+                entry.Complaint = entry.Complaint.ToLower();
+                entry.Problem = entry.Problem.ToLower();
+                if (!manipulator.AddDataEntry(CompanyId, entry, validated: true))
+                    Console.WriteLine(JsonDataObjectUtil<JobDataEntry>.ConvertObject(entry) + " was not added to the database");
+            }
+        }
+    }
+}
