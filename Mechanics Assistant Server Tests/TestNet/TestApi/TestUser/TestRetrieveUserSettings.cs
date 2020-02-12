547b1539eb6ca3fcecddb5e33470cd66a66a75c1
Freitacr <freitacr@plu.edu>
2020-02-12 11:31:27 -0800

    Stub Methods for Testing User Settings Api

---
+++ Mechanics Assistant Server Tests/TestNet/TestApi/TestUser/TestRetrieveUserSettings.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OldManInTheShopServer.Data.MySql;
+using OldManInTheShopServer.Data.MySql.TableDataTypes;
+using OldManInTheShopServer.Net.Api;
+using OldManInTheShopServer.Util;
+
+namespace MechanicsAssistantServerTests.TestNet.TestApi.TestUser
+{
+    [TestClass]
+    public class TestRetrieveUserSettings
+    {
+
+        private static UserSettingsApi TestApi;
+
+        [ClassInitialize]
+        public static void InitializeClass(TestContext ctx)
+        {
+            if (!TestingDatabaseCreationUtils.InitializeDatabaseSchema())
+                throw new Exception("Failed to initialize database schema. See logged error for details");
+            TestApi = new UserSettingsApi(10000);
+            if (!TestingDatabaseCreationUtils.InitializeUsers())
+                throw new Exception("Failed to initialize users in database. See logged error for details");
+        }
+
+        [ClassCleanup]
+        public static void CleanupTests()
+        {
+            ServerTestingMessageSwitchback.CloseSwitchback();
+            if (!TestingDatabaseCreationUtils.DestoryDatabase())
+                throw new Exception("Failed to destroy testing database. This is bad. Manual cleanup is required");
+        }
+
+        [TestMethod]
+        public void TestRetrieveUserSettingsDefaultValues()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestRetrieveUserSettingsModifiedSettings()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestBadRequestOnInvalidUserId()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestBadRequestOnEmptyLoginToken()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestUnauthorizedOnNonLoggedInUser()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestNotFoundOnNonExistentUser()
+        {
+
+        }
+
+
+
+    }
+}
---
+++ Mechanics Assistant Server Tests/TestNet/TestApi/TestUser/TestUpdateUserSettingValue.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OldManInTheShopServer.Data.MySql;
+using OldManInTheShopServer.Data.MySql.TableDataTypes;
+using OldManInTheShopServer.Net.Api;
+using OldManInTheShopServer.Util;
+
+namespace MechanicsAssistantServerTests.TestNet.TestApi.TestUser
+{
+    class TestUpdateUserSettingValue
+    {
+
+        private static UserSettingsApi TestApi;
+
+        [ClassInitialize]
+        public static void InitializeClass(TestContext ctx)
+        {
+            if (!TestingDatabaseCreationUtils.InitializeDatabaseSchema())
+                throw new Exception("Failed to initialize database schema. See logged error for details");
+            TestApi = new UserSettingsApi(10000);
+            if (!TestingDatabaseCreationUtils.InitializeUsers())
+                throw new Exception("Failed to initialize users in database. See logged error for details");
+        }
+
+        [ClassCleanup]
+        public static void CleanupTests()
+        {
+            ServerTestingMessageSwitchback.CloseSwitchback();
+            if (!TestingDatabaseCreationUtils.DestoryDatabase())
+                throw new Exception("Failed to destroy testing database. This is bad. Manual cleanup is required");
+        }
+
+        [TestMethod]
+        public void TestUpdateUserSetting()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestBadRequestOnInvalidUserId()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestBadRequestOnEmptyLoginToken()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestBadRequestOnEmptyAuthToken()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestBadRequestOnEmptyKey()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestBadRequestOnEmptyValue()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestUnauthorizedOnNonLoggedInUser()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestUnauthorizedOnNonAuthenticatedUser()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestNotFoundOnNonExistentUser()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestNotFoundOnNonExistentSetting()
+        {
+
+        }
+
+
+
+    }
+}
